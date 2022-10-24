fad0b59ef6dafc1fcf22bd096e55134bba457298
Ashley Thompson <ashleythompson@Ashleys-MacBook-Air.local>
2022-10-24 16:37:21 -0400

    media queries for hero, contact form styling NOT FINAL

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,104 +1,8 @@
 import '../styles/App.css';
 import React from 'react';
 import { useRef } from 'react';
-import { validateEmail } from '../utils/helpers';
+// import { validateEmail } from '../utils/helpers';
 import emailjs from '@emailjs/browser';
-// import { Form } from 'react-bootstrap';
-
-// function Contact() {
-//   //return contact form page html
-//     // Create state variables for the fields in the form
-//     const [fullName, setFullName] = useState('');
-//     const [email, setEmail] = useState('');
-//     const [message, setMessage] = useState('');
-//     const [errorMessage, setErrorMessage] = useState('');
-
-//     const handleInputChange = (e) => {
-//       // Getting the value and name of the input which triggered the change
-//       const { target } = e;
-//       const inputType = target.name;
-//       const inputValue = target.value;
-
-//       if (inputType === 'email') {
-//         setEmail(inputValue);
-//       } else if (inputType === 'fullName') {
-//         setFullName(inputValue);
-//       } else {
-//         setMessage(inputValue);
-//       }
-//     };
-
-
-//     const handleFormSubmit = (e) => {
-//       // Preventing the default behavior of the form
-//       e.preventDefault();
-
-//       if(!fullName || !email || !message) {
-//         setErrorMessage("Please fill in the empty field")
-//         return;
-//       }
-//       //validate email, err if not valid
-//       if (!validateEmail(email)) {
-//         setErrorMessage('Please enter a vaslid email');
-//         //exit out of this code block if something is wrong so that the user can correct it
-//         return;
-//       }
-//       alert(`Thanks for your message! I will be in contact soon.`);
-
-//       // Clear out the input after a successful registration.
-//       setMessage('');
-//       setEmail('');
-//       setFullName('');
-//     };
-
-//     return (
-//       <>
-        
-//           <div>
-//           <p className="contactGreeting">Hello there! Please use the form below to contact me about work, collaboration, or to send me pictures of your dog 🐕</p>
-//           <form className="contactForm">
-//           <Col>
-//             <input
-//               value={email}
-//               name="email"
-//               onChange={handleInputChange}
-//               type="email"
-//               placeholder="email"
-//             />
-//           </Col>
-//           <Col>
-//             <input
-//               value={fullName}
-//               name="fullName"
-//               onChange={handleInputChange}
-//               type="text"
-//               placeholder="full name"
-//             />
-//           </Col>
-//           <Col> 
-//             <input
-//               value={message}
-//               name="message"
-//               onChange={handleInputChange}
-//               type="text"
-//               placeholder="type your message here"
-              
-//             />
-//           </Col>  
-          
-//             <button className="submitContact" type="button" onClick={handleFormSubmit}>Submit</button>
-//           </form>
-//           {errorMessage && (
-//             <div>
-//               <p className="errorText">Oops! Something went wrong. Please try again.</p>
-//             </div>
-//           )}
-//           </div>  
-//       </>   
-//     );
-//   }
-// // }
-
 
 export const Contact = () => {
   const form = useRef();
@@ -107,6 +11,7 @@ export const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
+  //parameters for emailJS from my service and template setup info
     emailjs.sendForm('service_a19t1c5', 'template_xbtj6mj', form.current, 'jCNgFEcrKqO14_8pH')
       .then((result) => {
           console.log(result.text);
@@ -116,15 +21,23 @@ export const Contact = () => {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
+<>
+    <div>
+    <p className="contactGreeting">
+    Hello there! Use the form below to drop me a line about collaboration or work opportunities, or to send me pictures of your dog 🐕 
+    </p>
+    </div>
+    {/* //name params MUST MATCH those in the template on emailJS site or it won't return that information (ie name, return email) in the email it sends. */}
+    <form className="contactForm" ref={form} onSubmit={sendEmail}>
+      <label className='formLabel'>Name</label>
       <input type="text" name="from_name" />
-      <label>Email</label>
+      <label className='formLabel'>Email</label>
       <input type="email" name="reply_to" />
-      <label>Message</label>
+      <label className='formLabel'>Message</label>
       <textarea name="message" />
-      <input type="submit" value="Send" />
+      <input type="submit" value="Send" className="submitBtn"/>
     </form>
+</>  
   );
 };
 
