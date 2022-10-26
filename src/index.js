b58e29ad0f25aa9319c901fdd68d9fa0154cbdd4
Ashley Thompson <ashleythompson@Ashleys-MacBook-Air.local>
2022-10-26 11:47:11 -0400

    style changes, favicon update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './styles/App.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <BrowserRouter>
